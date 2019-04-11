8f82c960a5cb579a664c8bf13c94047a3f44679f
llyptical <ggalvan1998@gmail.com>
2019-04-11 11:01:13 -0700

    Update1

---
+++ index.php
@@ -0,0 +1,94 @@
+<html>
+	<head>
+		<title>Hi</title>
+		<link href="https://fonts.googleapis.com/css?family=Questrial" rel="stylesheet">
+		<style>
+			body{
+				background: #f8f8f8!important;
+			}
+			
+			h1{
+				color: #1A3784;
+				font-size: 5em;
+				text-align: center;
+				font-family: 'Questrial', sans-serif;
+			}
+			
+			.header{
+				width: 100%;
+				background: #ddd;
+				position: fixed;
+				top: 0; 
+				left: 0;
+			}
+			
+			.header ul{
+				margin: 0;
+				list-style-type: none;
+				background: #1a3784;
+				padding: 0;
+				overflow: hidden;
+			}
+			
+			.header ul li{
+				display: block;
+				float: right;
+				text-align: center;
+				padding: 14px 16px;
+				transition: 0.4s;
+			}
+			
+			.header ul li a{
+				text-decoration: none;
+				color: #fff;
+				font-family: 'Questrial', sans-serif;
+				display: block;
+				font-size: 1.2em;
+			}
+			
+			.header ul li:hover{
+				background: #192673;
+			}
+			
+			.content{
+				margin-top: 3%;
+				height: 97%;
+				width: 100%;
+				border: 1px solid #00ff00;
+				padding: 0 !important;
+			}
+			
+			.banner{
+				height: 50%;
+				background-image: url("/img/backgr.jpg");
+				background-size: cover;
+				margin: 0;
+				border: 1px solid #ff0000;
+				width: 100%;
+				padding: 0;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="header">
+			<ul>
+				<li>
+					<a href="/">
+						Menu Item 2
+					</a>
+				</li>
+				<li>
+					<a href="/">
+						Menu Item 1
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class="content">
+			<div class="banner">
+				
+			</div>
+			<h1>Website</h1>
+		</div>
+	</body>
+</html>
